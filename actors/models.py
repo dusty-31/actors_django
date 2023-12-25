079e2c947840a24904b14d55fb01ada1f0f3adf9
Yuriy Rybak <jura.ryback007@gmail.com>
2023-12-25 14:56:57 +0200

    Added actor model and migration.

--- actors/models.py
+++ actors/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    biography = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} | ID: {self.id}'
