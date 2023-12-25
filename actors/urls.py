c24407dd7b31846308e4963ae1563afe1361a73b
Yuriy Rybak <jura.ryback007@gmail.com>
2023-12-25 16:03:35 +0200

    Added actor page about actor. Fixed actor views and templates.

--- actors/urls.py
+++ actors/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.index_view, name='index'),
     path('about/', views.about_view, name='about'),
     path('category/<int:pk>', views.category_view, name='category'),
+    path('post/<int:pk>', views.post_view, name='post')
 ]
--- actors/views.py
+++ actors/views.py
@@ -1,24 +1,13 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Actor
 
 
 def index_view(request: HttpRequest) -> HttpResponse:
     context = {
         'title': 'Homepage',
-        'actors': [
-            {
-                'name': 'Tom Hanks',
-                'biography': 'Tom Hanks is an American actor and filmmaker. He is known for his roles in films such as Forrest Gump, Saving Private Ryan, and Cast Away.'
-            },
-            {
-                'name': 'Meryl Streep',
-                'biography': 'Meryl Streep is an American actress. Regarded as one of the greatest actresses of her generation, she has received numerous accolades throughout her career.'
-            },
-            {
-                'name': 'Leonardo DiCaprio',
-                'biography': 'Leonardo DiCaprio is an American actor and film producer. He has been nominated for six Academy Awards and has won an Oscar for his role in The Revenant.'
-            },
-        ],
+        'actors': Actor.objects.all(),
         'category_selected': 0,
     }
     return render(request=request, template_name='actors/index.html', context=context)
@@ -26,7 +15,7 @@ def index_view(request: HttpRequest) -> HttpResponse:
 
 def about_view(request: HttpRequest) -> HttpResponse:
     context = {
-        'title': 'About'
+        'title': 'About',
     }
     return render(request=request, template_name='actors/about.html', context=context)
 
@@ -34,20 +23,16 @@ def about_view(request: HttpRequest) -> HttpResponse:
 def category_view(request: HttpRequest, pk: int) -> HttpResponse:
     context = {
         'title': f'Categories {pk}',
-        'actors': [
-            {
-                'name': 'Tom Hanks',
-                'biography': 'Tom Hanks is an American actor and filmmaker. He is known for his roles in films such as Forrest Gump, Saving Private Ryan, and Cast Away.'
-            },
-            {
-                'name': 'Meryl Streep',
-                'biography': 'Meryl Streep is an American actress. Regarded as one of the greatest actresses of her generation, she has received numerous accolades throughout her career.'
-            },
-            {
-                'name': 'Leonardo DiCaprio',
-                'biography': 'Leonardo DiCaprio is an American actor and film producer. He has been nominated for six Academy Awards and has won an Oscar for his role in The Revenant.'
-            },
-        ],
+        'actors': Actor.objects.all(),
         'category_selected': pk,
     }
     return render(request=request, template_name='actors/index.html', context=context)
+
+
+def post_view(request: HttpRequest, pk: int) -> HttpResponse:
+    actor = get_object_or_404(klass=Actor, pk=pk)
+    context = {
+        'title': f'Post about {actor.first_name} {actor.last_name}',
+        'post': actor,
+    }
+    return render(request=request, template_name='actors/post.html', context=context)
