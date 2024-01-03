5dd014939ff58ca438d748c36a056bdd66ff9bde
Yuriy Rybak <jura.ryback007@gmail.com>
2024-01-03 16:14:22 +0200

    Added user registration functionality.

--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,6 @@ app_name = 'users'
 urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', views.UserRegisterCreateView.as_view(), name='register'),
+    path('register_done/', views.UserRegisterDoneView.as_view(), name='register_done'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,28 @@
 from django.contrib.auth.views import LoginView
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+from django.views import View
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, RegisterUserForm
 
 
 class UserLoginView(LoginView):
     template_name = 'users/login.html'
     form_class = UserLoginForm
     extra_context = {'title': 'Login'}
+
+
+class UserRegisterCreateView(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('users:register_done')
+
+
+class UserRegisterDoneView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        context = {
+            'title': 'Registration successful',
+        }
+        return render(request=request, template_name='users/register_done.html', context=context)
